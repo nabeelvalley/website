653b30b734bd543e40cd3a08d3035b8de42055d0
Nabeel Valley <nabeelvalley@outlook.com>
2019-08-04 14:14:37 +0200

    :bug: try to use hash routing for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
 import Preloader from './Components/Preloader/Preloader';
 
