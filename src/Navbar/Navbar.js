fdc23d35c200ebac83305cdb3e5198061d8848c3
Nabeel Valley <nabeelvalley@outlook.com>
2019-09-19 23:00:02 +0200

    :bug: activeClassName on correct element

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,10 +6,10 @@ const Navbar = (props) => (
     <nav className="Navbar">
         <div className="logo" style={props.location.pathname === '/home' ? { visibility: 'hidden', width: 0, height: 0 } : {}}>Nabeel Valley</div>
         <ul className="linkGroup">
-            <li activeClassName="active" className="link"><NavLink to="/home">Home</NavLink></li>
-            <li activeClassName="active" className="link"><NavLink to="/blog">Blog</NavLink></li>
-            <li activeClassName="active" className="link"><NavLink to="/code">Code</NavLink></li>
-            <li activeClassName="active" className="link"><NavLink to="/about">About</NavLink></li>
+            <li className="link"><NavLink activeClassName="active" to="/home">Home</NavLink></li>
+            <li className="link"><NavLink activeClassName="active" to="/blog">Blog</NavLink></li>
+            <li className="link"><NavLink activeClassName="active" to="/code">Code</NavLink></li>
+            <li className="link"><NavLink activeClassName="active" to="/about">About</NavLink></li>
         </ul>
     </nav>
 )
