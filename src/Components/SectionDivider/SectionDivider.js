eb8612d658921bad9786d6d8c5d8dae945515f8c
Nabeel Valley <nabeelvalley@outlook.com>
2019-09-24 10:05:55 +0200

    :sparkles: SectionDivider Component

---
+++ src/Components/SectionDivider/SectionDivider.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './SectionDivider.css'
+
+const SectionDivider = ({ title }) => <div className="SectionDivider">
+    <h2 className="title"><span className="text">{title}</span></h2>
+</div>
+
+export default SectionDivider
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Landing from '../Components/Landing/Landing';
 import FeaturedPost from '../Components/FeaturedPost/FeaturedPost'
+import SectionDivider from '../Components/SectionDivider/SectionDivider'
 
 const Home = () => <div className="Home">
     <Landing />
@@ -11,6 +12,7 @@ const Home = () => <div className="Home">
         synops="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in dapibus mauris, vitae ultrices ipsum. Fusce eget massa lectus. Pellentesque non lorem."
         link="/home"
     />
+    <SectionDivider title="What's New?" />
 </div>
 
 export default Home
