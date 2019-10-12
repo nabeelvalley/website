2a3d9e0ff430e43f7b9ed691f4408a6ac81c9447
Nabeel Valley <nabeelvalley@outlook.com>
2019-10-12 22:36:36 +0200

    :sparkles: about page

--- src/About/About.js
+++ src/About/About.js
@@ -8,7 +8,6 @@ const About = () => <ContentPage title="About" subtitle="Software Developer, Stu
     <InfoBlock
         heading="Me"
         location="Pretoria, South Africa"
-        description="just a placeholder for now"
         date="'95"
         body="my name is nabeel valley, born in 1995 - pretoria, south africa\ni'm passionate about solving challenging problems in beautiful ways\nmy daily-doings involve writing code, graphic and ui design, taking pictures, and a bit of reading"
     />
@@ -16,29 +15,51 @@ const About = () => <ContentPage title="About" subtitle="Software Developer, Stu
     <SectionDivider title="Experience" />
 
     <InfoBlock
-        heading="Me"
+        heading="Mercedes-Benz"
         location="Pretoria, South Africa"
-        description="just a placeholder for now"
-        date="'95"
-        body="my name is nabeel valley, born in 1995 - pretoria, south africa\ni'm passionate about solving challenging problems in beautiful ways\nmy daily-doings involve writing code, graphic and ui design, taking pictures, and a bit of reading"
-        summaryAlignRight={true}
+        description="Web Developer"
+        date="Mar'19-Present"
+        body="web developer in the marketing/customer data analytics team working on optimizing customer experiences on web platforms and integrating with a variety 1st and 3rd party applications and supporting with data related operations\nmaking use of agile methodologies and delivering software in a three tier application infrastructure using primarily .net, node.js, react, and jenkins"
+        summaryAlignRight
     />
 
-    <PostListing textAlignRight={true}
-        link="/home"
-        image="/images/posts/image1.jpg"
-        title="blog post title"
-        date="15 September 2019"
-        synops="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in dapibus mauris, vitae ultrices ipsum. Fusce eget massa lectus. Pellentesque non lorem."
+    <InfoBlock
+        heading="IBM"
+        location="Johannesburg, South Africa"
+        description="Cloud Developer Advocate"
+        date="Sept'18-Feb'19"
+        body="focused on enabling developers on ibm cloud technologies, building poc applications and demos for prospective clients showcasing technologies such as artificial intelligence, internet of things, containers and microservices, devops tools and methodologies, blockchain and design thinking\nadditionally i also wrote an article on assembling a continious deployment pipeline for web applications and coordinated and assisted developers with the data science johannesburg meetup group and the mtn tad hack in 2018"
     />
 
-    <PostListing
-        image="/images/posts/image1.jpg"
-        title="blog post title"
-        date="15 September 2019"
-        synops="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in dapibus mauris, vitae ultrices ipsum. Fusce eget massa lectus. Pellentesque non lorem."
-        link="/home"
+    <InfoBlock
+        heading="Microsoft"
+        location="Johannesburg, South Africa"
+        description="AppFactory Intern"
+        date="Feb'18-Aug'18"
+        body="development of mobile-first progressive web applications for a mentorship program using angular and asp.net core for azure called lifexchange, and the microsoft cloud summit 2018 event information site as well as engaged in client reviews and workshops as well as project planning. researched topics on web design and development"
+        summaryAlignRight
     />
+
+    <SectionDivider title="Education" />
+
+    <InfoBlock
+        heading="University of Pretoria"
+        location="Pretoria, South Africa"
+        description="BEng(Mech)"
+        date="Jan'14-Dec'17"
+        body="bachelor's degree in mechanical engineering with a final year specialization in mechatronics and reasearcha research project focused on renewable energy, i also tutored engineering programming in python to second year students"
+    />
+
+    <InfoBlock
+        heading="Al Ghazali College"
+        location="Pretoria, South Africa"
+        description="National Senior Certificate"
+        date="Jan'14-Dec'17"
+        body="national senior certificate with mathematics, physical sciences, life sciences, and information technology along with english as my primary language and afrikaans as the second"
+        summaryAlignRight
+    />
+
+
 </ContentPage>
 
 export default About
