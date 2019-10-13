d7afde6bfbc3a47529cf06ddce6ae99fd0d00510
Nabeel Valley <nabeelvalley@outlook.com>
2019-10-13 15:22:27 +0200

    :sparkles: add manifest images

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,5 @@
+console.log('sw file loaded')
+
 importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js'
 )
@@ -22,7 +24,7 @@ if (workbox) {
             plugins: [
                 new workbox.expiration.Plugin({
                     maxEntries: 50,
-                    maxAgeSeconds: 2 * 24 * 60 * 60 // 2 Days
+                    maxAgeSeconds: 12 * 60 * 60 // 0.5 Days
                 })
             ]
         })
