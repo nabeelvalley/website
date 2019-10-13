f7a43c8c85bb38963b82cd93c205156b1adaa46e
Nabeel Valley <nabeelvalley@outlook.com>
2019-10-13 14:15:38 +0200

    :sparkles: add zwartkops gallery to blog page

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -3,12 +3,21 @@ import ContentPage from '../Components/ContentPage/ContentPage'
 import PostListing from '../Components/PostListing/PostListing'
 
 const Blog = () => <ContentPage title="Blog" subtitle="Rants and Ramblings">
-    <PostListing textAlignRight={true}
+    <PostListing
         image="/content/blog/2019/12-10/header.jpg"
         title="Looky, a wild HTML!"
         date="13 October 2019"
         synops="The first blog post, A quick journey through my design and development process"
         link="/blog/2019/12-10/looky-a-wild-html"
+        textAlignRight
+    />
+    <PostListing
+        image="/images/zwartkops/image12.jpg"
+        title="Zwartkops Gallery"
+        date="27 July 2019"
+        synops="A photo gallery of the Extreme Racing Festival at Zwartkops Raceway in Centurion"
+        link="/gallery/zwartkops"
+        linkText="View the Gallery"
     />
 </ContentPage>
 
--- src/Components/PostListing/PostListing.js
+++ src/Components/PostListing/PostListing.js
@@ -3,14 +3,14 @@ import AnyLink from '../AnyLink/AnyLink'
 
 import './PostListing.css'
 
-const PostListing = ({ image, title, date, synops, link, textAlignRight = false }) =>
+const PostListing = ({ image, title, date, synops, link, textAlignRight = false, linkText = "Read More" }) =>
     <div className={'PostListing grid text-' + (textAlignRight === true ? 'right' : 'left')}>
         <img src={image} className="image fill" alt="" />
         <div className="text">
             <h2 className="title">{title}</h2>
             <div className="date">{date}</div>
             <p className="synops">{synops}</p>
-            <p className="link"><AnyLink to={link}>Read More</AnyLink></p>
+            <p className="link"><AnyLink to={link}>{linkText}</AnyLink></p>
         </div>
     </div>
 
