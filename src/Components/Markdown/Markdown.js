dac90abfc2484fd4b84f4d341d55b8958ccf7cff
Nabeel Valley <nabeelvalley@outlook.com>
2020-01-17 22:10:00 +0200

    :sparkles: markdown render with graphql

--- src/Components/Markdown/Markdown.js
+++ src/Components/Markdown/Markdown.js
@@ -1,41 +1,7 @@
 import React from 'react'
-import { Converter } from 'showdown'
-import * as showdownHighlighter from 'showdown-highlight'
 
 import './Markdown.css'
 
-const externalLinksInNewWindow = {
-    type: 'output',
-    regex: /<a\shref[^>]+>/g,
-    replace: (text) => {
-        var url = text.match(/"(.*?)"/)[1]
-        if (url.includes('http://') || url.includes('https://')) {
-            return '<a href="' + url + '" target="_blank" rel="noopener noreferrer">'
-        }
-        return text
-    }
-}
-
-const scrollableTables = {
-    type: 'output',
-    regex: /<table[^>]*>(?:.|\n)*?<\/table>/,
-    replace: (text) => `<div class="scrollable">${text}</div>`
-}
-
-const convertMarkdownToHtml = (text) => {
-    const converter = new Converter({
-        headerLevelStart: 2,
-        parseImgDimensions: true,
-        extensions: [showdownHighlighter, externalLinksInNewWindow, scrollableTables],
-        simplifiedAutoLink: true,
-        tables: true,
-        ghCompatibleHeaderId: true,
-        disableForced4SpacesIndentedSublists: true
-    })
-    const html = converter.makeHtml(text)
-    return html
-}
-
-const Markdown = ({ text }) => <div className="Markdown" dangerouslySetInnerHTML={{ __html: convertMarkdownToHtml(text) }}></div>
+const Markdown = ({ html }) => <div className="Markdown" dangerouslySetInnerHTML={{ __html: html }}></div>
 
 export default Markdown
--- src/Post/Post.js
+++ src/Post/Post.js
@@ -4,76 +4,34 @@ import AnyLink from '../Components/AnyLink/AnyLink'
 import ContentPage from '../Components/ContentPage/ContentPage'
 import Preloader from '../Components/Preloader/Preloader'
 import Markdown from '../Components/Markdown/Markdown'
+import Layout from '../../src/Layout'
+import { graphql } from 'gatsby'
 
 import './Post.css'
 
-class Post extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            slug: props.location.pathname,
-            content: '',
-            subtitle: '',
-            title: '',
-            description: '',
-            metaLoadingError: false,
-            contentLoadingError: false,
-            contentLoaded: false
-        }
-    }
-
-    scrollTop = () => window.scrollTo(0, 0)
-
-    getPostContent = async () => {
-        try {
-            const res = await fetch(`/content${this.state.slug}.json`)
-            const json = await res.json()
-            this.setState({ ...this.state, ...json }, this.scrollTop)
-        } catch (error) {
-            this.setState({ ...this.state, metaLoadingError: true, subtitle: "Page not found", title: "404" }, this.scrollTop)
-            console.error('failed to load post meta')
-        }
-
-        try {
-            const res = await fetch(`/content${this.state.slug}.md`)
-            const content = await res.text()
-            this.setState({ ...this.state, content })
-        } catch (error) {
-            this.setState({ ...this.state, contentLoadingError: true }, this.scrollTop)
-            console.error('failed to load post content')
-        } finally {
-            this.setState({ ...this.state, contentLoaded: true }, this.scrollTop)
-        }
-    }
-
-    async componentDidMount() {
-        this.setState({ ...this.state, contentLoaded: false }, this.getPostContent)
-    }
-
-    render() {
-        return
-        <Layout>
-            <div className="Post">
-                <ContentPage location={location} title={this.state.title || '...'} subtitle={this.state.subtitle || 'loading'}>
-                    {
-                        !this.state.contentLoaded
-                            ? <Helmet>
-                                <title>{this.state.title} | Nabeel Valley</title>
-                                <meta name="description" content={this.state.description} />
-                            </Helmet>
-                            : null
-                    }
-                    {
-                        !this.state.contentLoaded
-                            ? <Preloader />
-                            : !this.state.contentLoadingError && !this.state.metaLoadingError
-                                ? < Markdown text={this.state.content} />
-                                : <p>I can't seem to find the page you're looking for, but you may as well <AnyLink to="/">take a look around</AnyLink> since you're here anyway right?</p>
-                    }
-                </ContentPage>
-            </div>
-        </Layout>
-    }
-}
+const Post = ({ data, location }) => <Layout>
+    <div className="Post">
+        <ContentPage location={location} title={data.renderedMarkdownPost.title} subtitle={data.renderedMarkdownPost.subtitle || 'loading'}>
+            <Helmet>
+                <title>{data.renderedMarkdownPost.title} | Nabeel Valley</title>
+                <meta name="description" content={data.renderedMarkdownPost.description} />
+            </Helmet>
+            <Markdown html={data.renderedMarkdownPost.html} />
+        </ContentPage>
+    </div>
+</Layout >
 
 export default Post
+
+export const query = graphql`
+    query ($slug: String!) {
+        renderedMarkdownPost(slug: {eq: $slug}) {
+            id
+            description
+            html
+            title
+            subtitle
+            slug
+        }
+    }
+`
